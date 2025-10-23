39533a03f8ef10a332ff301a86657a3e81de8450
Ibrahim <ib.8686299@gmail.com>
2025-10-24 00:33:43 +0500

    first complete functionsl commit

--- app.js
+++ app.js
@@ -1,6 +1,82 @@
 let data;
+const body = document.querySelector("body");
+const boxContainer = document.querySelector(".box-container");
+const ayahPage = document.querySelector(".ayah-page");
+const ayah = document.querySelector(".ayah");
+const urdu = document.querySelector(".urdu");
+const english = document.querySelector(".english");
+const transliteration = document.querySelector(".roman");
+const ayahReference = document.querySelector("#ayahLabel");
+const tafsir = document.querySelector(".tafsir");
+const closePopup = document.querySelector(".ayah-close");
 
-// (async function(){
-//     data =  await axios.get("./moods.json");
-//     console.log(data);
-// })()
+(async function(){
+    data =  await axios.get("./moods.json");
+})();
+
+const boxes = document.querySelectorAll(".box");
+for(box of boxes){
+    box.addEventListener("click", handleBoxClick);
+}
+
+function handleBoxClick(event){
+    let moodType = this.classList[1];
+    switch (moodType){
+        case "sad":
+            showAyah(moodType);
+            break;
+        case "anxiety":
+            showAyah(moodType);
+            break;
+        case "irritability":
+            showAyah(moodType);
+            break;
+        case "lability":
+            showAyah(moodType);
+            break;
+        case "apathy":
+            showAyah(moodType);
+            break;
+        case "hopeless":
+            showAyah(moodType);
+            break;
+        case "overwhelm":
+            showAyah(moodType);
+            break;
+        case "loneliness":
+            showAyah(moodType);
+            break;
+        case "guilt":
+            showAyah(moodType);
+            break;
+        case "numbness":
+            showAyah(moodType);
+            break;
+        default:
+            console.log("is never possible");
+    }
+}
+
+function showAyah(mood){
+    console.log(data.data.moods[mood][0].arabic);
+    console.log(data.data.moods[mood][0].translation_ur.text);
+    console.log(data.data.moods[mood][0].translation_en.text);
+
+    ayahReference.innerText = `Key ${data.data.moods[mood][0].key}`
+    ayah.innerText = data.data.moods[mood][0].arabic;
+    transliteration.innerText = data.data.moods[mood][0].transliteration;
+    urdu.innerText = data.data.moods[mood][0].translation_ur.text;
+    english.innerText = data.data.moods[mood][0].translation_en.text;
+    tafsir.innerHTML = `<a class="tafsir-link" href="${data.data.moods[mood][0].tafsir_link}">${data.data.moods[mood][0].tafsir_snippet}</a>` 
+
+    // boxContainer.classList.toggle("hidden");
+    ayahPage.classList.toggle("hidden");
+    body.classList.toggle("active-popup");
+}
+
+closePopup.addEventListener("click", ()=>{
+    console.log("cloce clicked")
+    // boxContainer.classList.toggle("hidden");
+    ayahPage.classList.toggle("hidden");
+    body.classList.toggle("active-popup");
+})
